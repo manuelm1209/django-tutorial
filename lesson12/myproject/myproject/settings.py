570a3fc17db1e24c109bd19bb8869b64678b61bf
Manuel Montoya <manuelm1209@hotmail.com>
2024-12-17 20:02:28 -0500

    Setting up settings.py for deployment

--- lesson12/myproject/myproject/settings.py
+++ lesson12/myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-kyjo_5!%b==)c%mn-psi^9o&^aug*z__*i9%6bonary!$#$%j-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
@@ -120,15 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
-# 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = BASE_DIR / "assets"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
