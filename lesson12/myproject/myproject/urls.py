581baa614d119157afce9ec8ee3f7ffe6b808f2e
Manuel Montoya <manuelm1209@hotmail.com>
2024-12-17 20:15:39 -0500

    modifying the main ulrs.py file for deployment

--- lesson12/myproject/myproject/urls.py
+++ lesson12/myproject/myproject/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path # re_path = regular expresions
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path("admin/", admin.site.urls),
     path("", views.homepage),
     path("about/", views.about),
@@ -13,4 +16,4 @@ urlpatterns = [
     path("users/", include("users.urls"))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
